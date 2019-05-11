806aba1178e36e803fd3570c8f7a39640d03c377
satoshi.tsuboi <hstazkiki@gmail.com>
2019-05-11 19:22:58 +0900

    Add Employee show

--- src/controllers/employees/EmployeesShowServlet.java
+++ src/controllers/employees/EmployeesShowServlet.java
@@ -3,12 +3,14 @@ package controllers.employees;
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
 import utils.DBUtil;
 
 /**
@@ -32,6 +34,14 @@ public class EmployeesShowServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       EntityManager em = DBUtil.createEntityManager();
 
+      Employee e = em.find(Employee.class, Integer.parseInt(request.getParameter("id")));
 
+      em.close();
+
+      request.setAttribute("employee",e);
+      request.setAttribute("_token",request.getSession().getId());
+
+       RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/employees/show.jsp");
+         rd.forward(request,response);
 }
 }
